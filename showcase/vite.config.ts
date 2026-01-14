a2f42659bf3e94a314451048ea5949c23662a8c8
Jessica Mulein <jessica@digitaldefiance.org>
2026-01-14 08:10:31 -0800

    fix showcase

--- showcase/vite.config.ts
+++ showcase/vite.config.ts
@@ -1,11 +1,11 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/secrets-ts",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
   },
 });
